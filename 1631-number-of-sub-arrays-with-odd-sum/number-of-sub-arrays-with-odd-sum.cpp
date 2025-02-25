1b6062691e560c81eda62fb914b3599685e4c1d2
SPrashanth25618 <mail2sprashanth@gmail.com>
2025-02-25 21:15:22 +0530

    Time: 7 ms (43.49%) | Memory: 111.9 MB (80.00%) - LeetSync

--- 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.cpp
+++ 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.cpp
@@ -2,25 +2,14 @@ class Solution {
 public:
     int m = 1e9+7;
     int numOfSubarrays(vector<int>& arr) {
-        int ans = 0,n = arr.size();
-        vector<int> presum(n);
-        presum[0] = arr[0];
-        for(int i=1;i<n;i++)
-            presum[i] = presum[i-1] + arr[i];
-        int even = 1,odd = 0;
-        for(int i=0;i<n;i++)
+        int n = arr.size();
+        long long sum = 0,odd = 0;
+        for(int &i:arr)
         {
-            if(presum[i]%2!=0)
-            {
-                ans = (ans + even)%m;
-                odd++;
-            }
-            else
-            {
-                even++;
-                ans = (ans + odd)%m;
-            }
+            sum += i;
+            odd += sum%2;
         }
-        return ans;
+        odd += (n-odd)*odd;
+        return odd%m;
     }
 };
