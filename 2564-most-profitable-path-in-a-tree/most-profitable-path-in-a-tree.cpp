f1e68ec9fa8a06855b4065123246d3c23982317b
SPrashanth25618 <mail2sprashanth@gmail.com>
2025-02-26 21:21:19 +0530

    Time: 203 ms (40.14%) | Memory: 173 MB (29.52%) - LeetSync

--- 2564-most-profitable-path-in-a-tree/most-profitable-path-in-a-tree.cpp
+++ 2564-most-profitable-path-in-a-tree/most-profitable-path-in-a-tree.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     unordered_map<int,vector<int>> adj;
     unordered_map<int,int> mp;
-    int max_amount;
-    bool __bob(int cur,int time,vector<bool>& visited)
+    int tot_income;
+
+    bool DFS_bob(int cur,int time,vector<bool>& visited)
     {
         visited[cur] = true;
         mp[cur] = time;
         if(cur == 0)
             return true;
-        for(int& dg : adj[cur])
+        for(int f:adj[cur])
         {
-            if(!visited[dg] && __bob(dg,time + 1,visited))
+            if(!visited[f] && DFS_bob(f,time+1,visited))
                 return true;
         }
         mp.erase(cur);
@@ -19,46 +20,46 @@ public:
     }
 
     void Alice(int cur,int time,int income,vector<bool>& visited,vector<int>& amount)
-    {
+    {   
         visited[cur] = true;
         if(mp.find(cur) == mp.end() || time < mp[cur])
         {
             income += amount[cur];
         }
-        else if(time == mp[cur])
-        {
+        else if(time == mp[cur]){
             income += amount[cur]/2;
         }
 
         if(adj[cur].size() == 1 && cur != 0)
+            tot_income = max(tot_income,income);
+        
+        for(int i:adj[cur])
         {
-            max_amount = max(max_amount,income);
-        }
-
-        for(int& dg:adj[cur])
-        {
-            if(!visited[dg])
-                Alice(dg,time + 1,income,visited,amount);
+            if(!visited[i])
+            {
+                Alice(i,time+1,income,visited,amount);
+            }
         }
     }
 
     int mostProfitablePath(vector<vector<int>>& edges, int bob, vector<int>& amount) {
         int n = edges.size();
-        for(vector<int>& edg:edges)
+        tot_income = INT_MIN;
+        for(vector<int>& edg : edges)
         {
             int u = edg[0];
             int v = edg[1];
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-        max_amount = INT_MIN;
-        int time = 0;
+        
         vector<bool> visited(n,false);
-        __bob(bob,time,visited);
+        int time = 0;
+        DFS_bob(bob,time,visited);
 
-        int income = 0;
         visited.assign(n,false);
+        int income = 0;
         Alice(0,0,income,visited,amount);
-        return max_amount;
+        return tot_income;
     }
 };
