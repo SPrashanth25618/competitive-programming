d255900c4b49cda7de8224655246940062e885b7
SPrashanth25618 <mail2sprashanth@gmail.com>
2025-04-11 14:58:18 +0530

    Time: 3 ms (72.88%) | Memory: 14.9 MB (21.27%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -6,8 +6,9 @@ public:
         int n = nums.size();
         for(int i=0;i<n;i++)
         {
-            if( mp.find(target-nums[i]) != mp.end() )
-                return {mp[target-nums[i]],i};
+            int x=target - nums[i];
+            if( mp.find(x) != mp.end() )
+                return {mp[x],i};
             mp[nums[i]] = i;
         }
         return {};
