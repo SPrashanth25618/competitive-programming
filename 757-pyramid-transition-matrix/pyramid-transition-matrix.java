7f332e800e17e084e142996050aa3d642ee467c5
Prashanth <mail2sprashanth@gmail.com>
2025-12-29 18:21:10 +0530

    Time: 338 ms (59.87%) | Memory: 143.9 MB (5.10%) - LeetSync

--- 757-pyramid-transition-matrix/pyramid-transition-matrix.java
+++ 757-pyramid-transition-matrix/pyramid-transition-matrix.java
@@ -1,4 +1,5 @@
 class Solution {
+    private Map<String,Boolean> dp = new HashMap<>();
     public boolean pyramidTransition(String bottom, List<String> allowed) {
         Map<String,List<Character>> mp = new HashMap<>();
         for(String s:allowed){
@@ -6,38 +7,36 @@ class Solution {
             char ch = s.charAt(2);
             mp.computeIfAbsent(pair,k -> new ArrayList<>()).add(ch);
         }        
-        /*
-        for(Map.Entry<String,List<Character>> e : mp.entrySet()){
-            String pair = e.getKey();
-            List<Character> val = e.getValue();
-            System.out.println(pair+" "+val);
-        }
-
-        another way of traversing the map
-        mp.forEach((key,value) -> {
-            System.out.println(key+" "+value);
-        });
-        */
         return fun(bottom,mp,0,new StringBuilder());
     }
     public boolean fun(String cur,Map<String,List<Character>> mp,int idx,StringBuilder next){
         if(cur.length() == 1)
             return true;
+        String key = cur+"_"+idx+"_"+next;
+        if(dp.containsKey(key))
+            return dp.get(key);
+
         if(idx == cur.length()-1){
-            return fun(next.toString(),mp,0,new StringBuilder());
+            boolean res = fun(next.toString(),mp,0,new StringBuilder());
+            dp.put(key,res);
+            return res;
         }
 
         String pair = cur.substring(idx,idx+2);
         if(!mp.containsKey(pair)){
+            dp.put(key,false);
             return false;
         }
 
         for(char ch:mp.get(pair)){
             next.append(ch);
-            if(fun(cur,mp,idx+1,next))
+            if(fun(cur,mp,idx+1,next)){
+                dp.put(key,true);
                 return true;
+            }
             next.deleteCharAt(next.length()-1);
         }
+        dp.put(key,false);
         return false;
     }
 }
