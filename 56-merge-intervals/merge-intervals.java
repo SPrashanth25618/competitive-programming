0c7117eaeda32f355bcb8cf3b99239d81cf85105
Prashanth <mail2sprashanth@gmail.com>
2026-01-02 16:51:51 +0530

    Time: 11 ms (17.16%) | Memory: 49.2 MB (36.98%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -2,22 +2,14 @@ class Solution {
     public int[][] merge(int[][] ar) {
         int n = ar.length;
         Arrays.sort(ar,(a,b) -> a[0]-b[0]);
-        List<List<Integer>> ans = new ArrayList<>();
-        for(int i=0;i<n;i++){
-            int st = ar[i][0],ed = ar[i][1];
-            if(ans.isEmpty() || st > ans.get(ans.size()-1).get(1)){
-                ans.add(Arrays.asList(st,ed));
+        List<int[]> ans = new ArrayList<>();
+        for(int[] i:ar){
+            if(ans.isEmpty() || i[0] > ans.get(ans.size()-1)[1]){
+                ans.add(new int[]{i[0],i[1]});
             }else{
-                ans.get(ans.size()-1).set(1,Math.max(ans.get(ans.size()-1).get(1),ed));
+                ans.get(ans.size()-1)[1] = Math.max(ans.get(ans.size()-1)[1],i[1]);
             }
-        }
-        System.out.println(ans);
-        int[][] res = new int[ans.size()][2];
-        for(int i=0;i<ans.size();i++){
-            for(int j=0;j<2;j++){
-                res[i][j] = ans.get(i).get(j);
-            }
-        }
-        return res;
+        }        
+        return ans.toArray(new int[ans.size()][2]);
     }
 }
