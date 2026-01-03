c50483567dab5f29b15208b76e16362a72116e39
Prashanth <mail2sprashanth@gmail.com>
2026-01-03 10:56:42 +0530

    Time: 1 ms (24.04%) | Memory: 43.6 MB (87.78%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,20 +1,33 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m-1,j = 0;
-        while(i>=0 && j<n){
-            if(nums1[i] > nums2[j]){
-                int t = nums1[i];
-                nums1[i] = nums2[j];
-                nums2[j] = t;
-                i--;
+        int len = m+n;
+        int gap = (len/2) + (len%2);
+        while(gap > 0){
+            int i=0,j=i+gap;
+            while(j<len){
+                if(i<m && j>=m){
+                    swap(nums1,nums2,i,j-m);
+                }else if(i>=m){
+                    swap(nums2,nums2,i-m,j-m);
+                }else{
+                    swap(nums1,nums1,i,j);
+                }
+                i++;
                 j++;
-            }else{
-                break;
             }
+            if(gap == 1)
+                break;
+            gap = (gap/2)+(gap%2);
         }
-        for(int k = m;k<m+n;k++){
+        for(int k=m;k<n+m;k++){
             nums1[k] = nums2[k-m];
         }
-        Arrays.sort(nums1);        
+    }
+    public void swap(int[] ar1,int[] ar2,int i,int j){
+        if(ar1[i] > ar2[j]){
+            int t = ar1[i];
+            ar1[i] = ar2[j];
+            ar2[j] = t;
+        }
     }
 }
