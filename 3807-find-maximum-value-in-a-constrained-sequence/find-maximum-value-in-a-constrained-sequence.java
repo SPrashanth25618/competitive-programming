3251eaba8c34f3051187c8d1cd710021295b829a
Prashanth <mail2sprashanth@gmail.com>
2026-01-04 10:13:19 +0530

    Time: 6 ms (99.51%) | Memory: 208 MB (30.98%) - LeetSync

---
+++ 3807-find-maximum-value-in-a-constrained-sequence/find-maximum-value-in-a-constrained-sequence.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findMaxVal(int n, int[][] restrictions, int[] diff) {
+        int[] a = new int[n];
+        Arrays.fill(a,Integer.MAX_VALUE);
+        a[0] = 0;
+        for(int[] i:restrictions)
+            a[i[0]] = Math.min(a[i[0]],i[1]);        
+        for(int i=1;i<n;i++)
+            a[i] = Math.min(a[i],a[i-1]+diff[i-1]);        
+        for(int i=n-2;i>=0;i--)
+            a[i] = Math.min(a[i],a[i+1]+diff[i]);        
+        int maxi = Integer.MIN_VALUE;
+        for(int i:a)
+            maxi = Math.max(maxi,i);
+        return maxi;
+    }
+}
